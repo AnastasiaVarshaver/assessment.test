61358f37ea55f9781434d2de62da63a9c98f60ea
AnastasiaVarshaver <80210939+AnastasiaVarshaver@users.noreply.github.com>
2021-07-01 13:00:17 +0300

    Update lol.regions.page.ts

--- features/pageObjects/lol.regions.page.ts
+++ features/pageObjects/lol.regions.page.ts
@@ -17,7 +17,6 @@ export class mobalyticsRegion {
     public async RegionButton (){
         await browser.wait(ExpectedConditions.visibilityOf(await this.mobalyticsRegion.regionsRU), defaultTimeout, "Pop-up didn't load or has incorrect locator");
         await browser.actions().click(this.mobalyticsRegion.regionsRU).perform();
-        await browser.sleep(2000);
     }
 
 
@@ -25,7 +24,6 @@ export class mobalyticsRegion {
         await browser.wait(ExpectedConditions.visibilityOf(await this.mobalyticsRegion.regionsPopup), defaultTimeout, "EUW option didn't load or has incorrect locator");
         await browser.wait(ExpectedConditions.visibilityOf(await this.mobalyticsRegion.regionEUW), defaultTimeout, "EUW option didn't load or has incorrect locator");
         await browser.actions().click(this.mobalyticsRegion.regionEUW).perform();
-        await browser.sleep(2000);
     }
     public async Result (){
         await browser.wait(ExpectedConditions.visibilityOf(await this.mobalyticsRegion.regionEUW2), defaultTimeout, "Changes weren't applied");
